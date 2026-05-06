5bbc047419c2aa18b7b9fca67969af4c3c3e966c
Thiago Costa <admin@rootbsd.info>
2026-05-06 22:19:37 +0000

    Update __init__.py to import all widgets.

--- src/textual_calendar/__init__.py
+++ src/textual_calendar/__init__.py
@@ -1,5 +1,13 @@
 
-from .calendar import Calendar
+from .calendar import Calendar, CalendarDate, CalendarMonth
+from .time import Time
+from .datetime import DatetimePicker
 
 __version__ = "1.0.0"
-__all__ = ["Calendar"]
+__all__ = [
+    "Calendar",
+    "CalendarDate",
+    "CalendarMonth",
+    "Time",
+    "DatetimePicker",
+]
